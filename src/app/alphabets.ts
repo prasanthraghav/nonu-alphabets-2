ea45e263ae87f1741ed50e28b4f52e39b2784847
Prasanth Raghavan <devsoft575@gmail.com>
2025-09-18 22:55:24 +0530

    feat: add event handlers # add click event handler # add keyboard event handler

---
+++ src/app/alphabets.ts
@@ -0,0 +1,108 @@
+const alphabets = {
+  A: {
+    word: 'Apple',
+    image: 'apple.jpeg'
+  },
+  B: {
+    word: 'Ball',
+    image: 'ball.jpeg'
+  },
+  C: {
+    word: 'Cat',
+    image: 'cat.jpeg'
+  },
+  D: {
+    word: 'Dog',
+    image: 'dog.jpeg'
+  },
+  E: {
+    word: 'Elephant',
+    image: 'elephant.jpeg'
+  },
+  F: {
+    word: 'Fish',
+    image: 'fish.jpeg'
+  },
+  G: {
+    word: 'Grapes',
+    image: 'grapes.jpeg'
+  },
+  H: {
+    word: 'Hat',
+    image: 'hat.jpeg'
+  },
+  I: {
+    word: 'Ice Cream',
+    image: 'icecream.jpeg'
+  },
+  J: {
+    word: 'Juice',
+    image: 'juice.jpeg'
+  },
+  K: {
+    word: 'Kite',
+    image: 'kite.jpeg'
+  },
+  L: {
+    word: 'Lion',
+    image: 'lion.jpeg'
+  },
+  M: {
+    word: 'Monkey',
+    image: 'monkey.jpeg'
+  },
+  N: {
+    word: 'Nest',
+    image: 'nest.jpeg'
+  },
+  O: {
+    word: 'Orange',
+    image: 'orange.jpeg'
+  },
+  P: {
+    word: 'Pencil',
+    image: 'pencil.jpeg'
+  },
+  Q: {
+    word: 'Quail',
+    image: 'quail.jpeg'
+  },
+  R: {
+    word: 'Rabbit',
+    image: 'rabbit.jpeg'
+  },
+  S: {
+    word: 'Sun',
+    image: 'sun.jpeg'
+  },
+  T: {
+    word: 'Tiger',
+    image: 'tiger.jpeg'
+  },
+  U: {
+    word: 'Umbrella',
+    image: 'umbrella.jpeg'
+  },
+  V: {
+    word: 'Violin',
+    image: 'violin.jpeg'
+  },
+  W: {
+    word: 'Whale',
+    image: 'whale.jpeg'
+  },
+  X: {
+    word: 'Xylophone',
+    image: 'xylophone.jpeg'
+  },
+  Y: {
+    word: 'Yacht',
+    image: 'yacht.jpeg'
+  },
+  Z: {
+    word: 'Zebra',
+    image: 'zebra.jpeg'
+  }
+};
+
+export default alphabets;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,143 @@
-import Image from 'next/image';
+'use client';
+
+import { useState } from 'react';
+import alphabets from './alphabets';
+import { get } from 'http';
+
+type AlphabetKey = keyof typeof alphabets;
 
 export default function Home() {
+  const [currentAlphabet, setCurrentAlphabet] = useState<AlphabetKey>('D');
+  const { word, image } = alphabets[currentAlphabet];
+  const imgUrl = `/images/alphabets/${image}`;
+
+  const getNextAlphabet = () => {
+    const ascii = currentAlphabet.charCodeAt(0);
+    const nextAlphabet = String.fromCharCode(
+      ((ascii - 65 + 1) % 26) + 65
+    ) as AlphabetKey;
+    setCurrentAlphabet(nextAlphabet);
+  };
+
+  const handleclick = () => {
+    getNextAlphabet();
+  };
+
+  const handleKeyPress = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'Enter':
+      case 'ArrowRight':
+      case ' ':
+      case 'Escape':
+      case 'Backspace':
+      case 'Tab':
+      case 'Shift':
+      case 'Control':
+      case 'Alt':
+      case 'Meta':
+      case 'CapsLock':
+      case 'F1':
+      case 'F2':
+      case 'F3':
+      case 'F4':
+      case 'F5':
+      case 'F6':
+      case 'F7':
+      case 'F8':
+      case 'F9':
+      case 'F10':
+      case 'F11':
+      case 'F12':
+      case 'ArrowLeft':
+      case 'ArrowUp':
+      case 'ArrowDown':
+        event.preventDefault();
+        getNextAlphabet();
+        break;
+
+      case 'a':
+      case 'b':
+      case 'c':
+      case 'd':
+      case 'e':
+      case 'f':
+      case 'g':
+      case 'h':
+      case 'i':
+      case 'j':
+      case 'k':
+      case 'l':
+      case 'm':
+      case 'n':
+      case 'o':
+      case 'p':
+      case 'q':
+      case 'r':
+      case 's':
+      case 't':
+      case 'u':
+      case 'v':
+      case 'w':
+      case 'x':
+      case 'y':
+      case 'z':
+        setCurrentAlphabet(event.key.toUpperCase() as AlphabetKey);
+        break;
+      case 'A':
+      case 'B':
+      case 'C':
+      case 'D':
+      case 'E':
+      case 'F':
+      case 'G':
+      case 'H':
+      case 'I':
+      case 'J':
+      case 'K':
+      case 'L':
+      case 'M':
+      case 'N':
+      case 'O':
+      case 'P':
+      case 'Q':
+      case 'R':
+      case 'S':
+      case 'T':
+      case 'U':
+      case 'V':
+      case 'W':
+      case 'X':
+      case 'Y':
+      case 'Z':
+        setCurrentAlphabet(event.key as AlphabetKey);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  if (typeof window !== 'undefined') {
+    window.onkeydown = handleKeyPress;
+  }
+
   return (
-    <main className="grid grid-cols-[2fr_3fr] h-full p-20">
+    <main
+      className="grid grid-cols-[2fr_3fr] h-full p-20"
+      onClick={handleclick}
+    >
       <div className="grid place-items-center">
         <div className="text-center">
-          <div className="">A</div>
-          <div>Apple</div>
+          <div className="text-9xl">{currentAlphabet}</div>
+          <div className="text-7xl mt-20">{word}</div>
         </div>
       </div>
       <div className="grid place-items-center">
         <div>
           <img
-            src="apple.jpeg"
-            alt="Apple"
-            className="max-w-full h-auto rounded-3xl"
-            width={400}
-            height={400}
+            src={imgUrl}
+            alt={word}
+            className="max-w-full h-auto rounded-3xl w-48 md:w-64 lg:w-128 h-auto"
           />
         </div>
       </div>
