8c3711e91c65bbab20b7a67727926494bbffed87
Prasanth Raghavan <devsoft575@gmail.com>
2025-09-20 09:53:00 +0530

    feat: add meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-screen">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
